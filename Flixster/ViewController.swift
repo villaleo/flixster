903c887a6cc6b166574d14a3fd7d858ea9457162
Leo Villalobos <villaleobos@gmail.com>
2023-02-14 15:01:56 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Leonardo Villalobos on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
