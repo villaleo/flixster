162890f4023fd53ce7c1c9b2f3dc2397f82fe074
Leo Villalobos <villaleobos@gmail.com>
2023-02-14 19:07:03 -0800

    Conform ViewController to UITableViewDataSource

--- Flixster/ViewController.swift
+++ Flixster/ViewController.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var movieTView: UITableView!
+    var movies: [Movie] = []
     let movieAPIKey: String? = KeyManager.instance.map["movieAPI"] as? String
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = movieTView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieTVCell
+        // TODO: Configure cell
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.movies = Movie.mockData
+        self.movieTView.dataSource = self
     }
 }
